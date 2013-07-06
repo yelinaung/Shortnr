599a49197e0ca09224bda9f60c30d59771ded33d
mgpyone <mgpyone@mysteryzillion.com>
2013-07-06 17:16:59 +0630

    Added Redis config and updated the gems

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require 'sinatra/reloader'
 require 'redis'
 require './helper'
 
+before do
+  @redis = Redis.new(:host => "localhost", :port => 6379, :password => nil)
+end
+
 get '/' do
   "This is index"
 end
@@ -16,6 +20,7 @@ post '/index' do
   @sht_string = "Hello World " + sht
 
   if (params[:url] =~ URI::regexp)
-    @ 
+    @token = sht
+  end
 end
 
